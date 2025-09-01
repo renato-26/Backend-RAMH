dedfa370c35e2323a8165e3d85adfc5cfcd72319
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-09-01 17:11:49 -0400

    avance actividad1

--- monitoreo/monitoreo/urls.py
+++ monitoreo/monitoreo/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 from dispositivos.views import inicio
+from dispositivos.views import panel_dispositivos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
