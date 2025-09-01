81d6b4352d57dc0fe0dbae5829c778b66941d541
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-09-01 18:06:23 -0400

    avance panel y clase 1/9

--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import dispositivos
+from .forms import dispositivosForm
 
 def inicio(request):
     contexto = {"nombre": "profe javier"}
@@ -13,3 +14,12 @@ def inicio(request):
         "contexto": contexto,
         "productos": productos
     })
+
+def dispositivos(request, dispositivo_id):
+    dispositivo = dispositivos.objects.get(
+        id=dispositivo_id
+    )
+    return render(request,
+                "dispostivos/dispositivos.html",
+                {"dispositivo": dispositivo}
+                )
