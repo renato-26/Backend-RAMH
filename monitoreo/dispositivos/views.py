24ea48a08727225d5d87c572cdf1794a9dfc01e7
renato-26 <munozrenato329@gmail.com>
2025-09-01 00:16:42 -0400

    avance ppt2 clase 3-4

--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def inicio(request):
+    contexto = {"nombre": "profe javier"}
+    productos =[
+        {"nombre": "producto1", "precio": 100},
+        {"nombre": "producto2", "precio": 200},
+        {"nombre": "producto3", "precio": 300}
+    ]
+
+    return render(request, "dispositivos/inicio.html", {
+        "contexto": contexto,
+        "productos": productos
+    })
